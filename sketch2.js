03e705cf95219eb698fcf1730c5e708174c321ef
Marko Vrljes <markovrljes11@gmail.com>
2023-04-15 16:20:48 -0400

    Made power ups, no timer though

--- sketch2.js
+++ sketch2.js
@@ -1,5 +1,6 @@
 // References
 // Sounds: https://mixkit.co/free-sound-effects/game/?page=2
+// Images: https://www.flaticon.com/search?type=icon&search-group=all&word=speed&license=&color=&shape=&current_section=&author_id=&pack_id=&family_id=&style_id=&choice=&type=
 
 // Styling variables
 const BACKGROUND_COLOR = "#222222";
@@ -14,13 +15,14 @@ let score = 0;
 let lives = 3;
 let level = 1;
 let bricks = [];
+let powerUps = [];
 let ball;
 let paddle;
 
 // Asset variables
 let blockHit;
 let paddleHit;
-let powerUp;
+let powerUpSound;
 
 // our game will have 3 possible 'states':
 const INTRO = 0;
@@ -30,7 +32,6 @@ const GAME_OVER = 2;
 // we start in the INTRO state
 let gameState = INTRO;
 
-
 /////////////////////////////////////////////////////////////////
 
 // Code placed in setup() will run once at the beginning
@@ -38,7 +39,6 @@ function setup() {
   // Create a new canvas to match the browser size
   createCanvas(windowWidth, windowHeight / 2);
   //imageMode(CENTER);
-
 }
 
 /////////////////////////////////////////////////////////////////
@@ -47,7 +47,8 @@ function preload() {
   // load all of our games' assets in global variables
   blockHit = loadSound("Assets/blockHit.mp3");
   paddleHit = loadSound("Assets/paddleHit.wav");
-  powerUp = loadSound("Assets/powerUp.wav");
+  powerUpSound = loadSound("Assets/powerUp.wav");
+  song = loadSound("Assets/song.mp3");
 }
 
 /////////////////////////////////////////////////////////////////
@@ -78,22 +79,17 @@ function intro() {
   text("Brick Breaker", width / 2, height / 2 - 50);
   textSize(20);
   text(
-    "USE THE MOUSE TO MOUVE THE PADDLE TO HIT THE BALL AND DESTROY ALL BLOCKS",
+    "USE THE MOUSE TO MOVE THE PADDLE TO HIT THE BALL AND DESTROY ALL BLOCKS",
     width / 2,
     height / 2 + 10
   );
-  text(
-    "CLICK TO START",
-    width / 2,
-    height / 2 + 40
-  );
-
+  text("CLICK TO START", width / 2, height / 2 + 40);
 }
 
 /////////////////////////////////////////////////////////////////
 
 function runGame() {
-background(BACKGROUND_COLOR);
+  background(BACKGROUND_COLOR);
 
   // Draw the paddle
   paddle.show();
@@ -108,27 +104,38 @@ background(BACKGROUND_COLOR);
   textSize(20);
   textAlign(CENTER, CENTER);
   fill(TEXT_COLOR);
-  text("Score: " + score, 50, 30);
-  text("Lives: " + lives, width - 50, 30);
-  text("Press the paddle to release the ball", width / 2 , 30);
+  text("SCORE: " + score, 50, 30);
+  text("LIVES: " + lives, width - 50, 30);
 
-
-  for (let i = 0; i < bricks.length; i++) {
-    bricks[i].show();
-    bricks[i].checkCollision(ball, i);
+  // Check if the ball is still stuck to the paddle
+  if (ball.isAttached) {
+    text("PRESS THE PADDLE TO RELEASE THE BALL", width / 2, 30);
   }
 
-//   // Check for level complete
-//   if (bricks.length == 0) {
-//     nextLevel();
-//   }
+  // Show the bricks and check collision with ball and other elements
+  for (let i = 0; i < bricks.length; i++) {
+    let brick = bricks[i];
+    brick.show();
+    brick.checkCollision(ball, i);
+  }
+
+  // Update and render power-ups
+  for (let i = powerUps.length - 1; i >= 0; i--) {
+    let power = powerUps[i];
+    power.show();
+    power.move();
+    power.checkCollision(paddle, i);
+  }
+
+  // Check for level complete
+  if (bricks.length == 0) {
+    nextLevel();
+  }
 
   // Check for game over
-  if (lives <= 0 || bricks.length == 0) {
+  if (lives <= 0) {
     gameState = GAME_OVER;
   }
-
- 
 }
 
 /////////////////////////////////////////////////////////////////
@@ -143,8 +150,8 @@ function gameOver() {
   // Draw the score and lives
   textSize(20);
   fill(TEXT_COLOR);
-  text("Score: " + score, 10, 30);
-  text("Lives: " + lives, width - 80, 30);
+  text("SCORE: " + score, 10, 30);
+  text("LIVES: " + lives, width - 80, 30);
 
   fill("red");
   textSize(60);
@@ -162,7 +169,7 @@ function mousePressed() {
   if (gameState == INTRO) {
     resetGame();
     gameState = RUN_GAME;
-    // if (song.isLoaded()) song.loop();  // for song later add it
+    //if (song.isLoaded()) song.loop();  // find a good song
   }
   if (gameState == GAME_OVER) {
     gameState = RUN_GAME;
@@ -173,43 +180,41 @@ function mousePressed() {
 /////////////////////////////////////////////////////////////////
 
 function createBricks() {
-    let rows = level + 2;
-    let cols = 10;
-    let brickWidth = width / cols;
-    let brickHeight = 20;
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        let x = j * brickWidth;
-        let y = i * brickHeight + 50;
-        let color = BRICK_COLORS[i % BRICK_COLORS.length];
-        //let color = BRICK_COLORS[Math.floor(Math.random() * BRICK_COLORS.length)];
-        bricks.push(new Brick(x, y, brickWidth, brickHeight, color, 0));
-      }
+  let rows = level + 2;
+  let cols = 10;
+  let brickWidth = width / cols;
+  let brickHeight = 20;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let x = j * brickWidth;
+      let y = i * brickHeight + 50;
+      let color = BRICK_COLORS[i % BRICK_COLORS.length];
+      bricks.push(new Brick(x, y, brickWidth, brickHeight, color, 0));
     }
   }
-
+}
 
 /////////////////////////////////////////////////////////////////
 
 function resetGame() {
-    score = 0;
-    lives = 3;
-    level = 1;
-    bricks = [];
-    ball = new Ball();
-    paddle = new Paddle();
-    createBricks();
-
-  }
+  score = 0;
+  lives = 3;
+  level = 1;
+  bricks = [];
+  powerUps = [];
+  ball = new Ball();
+  paddle = new Paddle();
+  createBricks();
+}
 
 /////////////////////////////////////////////////////////////////
 
-// function nextLevel() {
-//   level++;
-//   ball = new Ball();
-//   paddle = new Paddle();
-//   createBricks();
-// }
+function nextLevel() {
+  level++;
+  ball = new Ball();
+  paddle = new Paddle();
+  createBricks();
+}
 
 /////////////////////////////////////////////////////////////////
 
@@ -261,17 +266,19 @@ class Ball {
     this.radius = 10;
     this.x = width / 2;
     this.y = height / 2;
+    this.color = BALL_COLOR;
     this.xSpeed = random(-5, 5);
     this.ySpeed = -6;
     this.isAttached = true; // new variable to keep track of whether the ball is attached to the paddle
   }
 
   show() {
-    fill(BALL_COLOR);
+    fill(this.color);
     circle(this.x, this.y, this.radius * 2);
   }
 
   move() {
+    // if user presses the paddle
     if (
       this.isAttached &&
       mouseIsPressed &&
@@ -308,6 +315,7 @@ class Ball {
       let distance = this.x - (paddle.x + paddle.width / 2);
       let normalizedDistance = distance / (paddle.width / 2);
       this.xSpeed = normalizedDistance * 5;
+      paddleHit.play();
     }
     // Check collision with walls
     if (this.x - this.radius <= 0 || this.x + this.radius >= width) {
@@ -331,16 +339,6 @@ class Ball {
   reverseY() {
     this.ySpeed *= -1;
   }
-
-  hits(brick) {
-    let distance = dist(
-      this.x,
-      this.y,
-      brick.x + brick.width / 2,
-      brick.y + brick.height / 2
-    );
-    return distance < this.radius + brick.width / 2;
-  }
 }
 
 /////////////////////////////////////////////////////////////////
@@ -353,28 +351,42 @@ class Brick {
     this.height = h;
     this.color = c;
     this.damage = d; // new variable to keep track of the number of hits the block has taken
+    if (Math.random() < 0.2) {
+      // adjust the probability of a power-up appearing here
+      this.hasPowerUp = true;
+    } else {
+      this.hasPowerUp = false;
+    }
   }
 
+  // Check collision with ball and bricks
   checkCollision(ball, blockNum) {
     if (
-      ball.x > this.x &&
-      ball.x < this.x + this.width &&
-      ball.y > this.y &&
-      ball.y < this.y + this.height
+      ball.y - ball.radius < this.y + this.height &&
+      ball.y + ball.radius > this.y &&
+      ball.x + ball.radius > this.x &&
+      ball.x - ball.radius < this.x + this.width
     ) {
-      ball.reverseY();
-      this.damage++;
+      ball.reverseY(); // reverse ball y direction
+      this.damage++; // increase brick damage
+      //this.hasPowerUp = false; // remove power-up from brick
+      blockHit.play();
       if (this.damage >= 2) {
         score += 10;
-        bricks.splice(blockNum, 1);
+        bricks.splice(blockNum, 1); // remove brick if damage is 2 and add score
+        if (this.hasPowerUp) {
+          //let powerUp = new PowerUp(this.x + this.width / 2 - 10, this.y + this.height);
+          let powerUp = new PowerUp(this.x, this.y);
+          powerUps.push(powerUp); // if the brick has a power up, add it to the powerUps array
+        }
       }
-      blockHit.play();
     }
   }
 
   show() {
     if (this.damage == 0) {
       fill(this.color);
+      strokeWeight(2);
       rect(this.x, this.y, this.width, this.height);
     } else {
       // Draw a cracked Brick
@@ -410,6 +422,75 @@ class Brick {
         this.x + this.width,
         this.y + this.height
       );
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////
+
+class PowerUp {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.img = null;
+    this.height = 20;
+    this.speed = 3; // adjust this value to change the speed of the power-up
+    this.type = floor(random(0, 5));
+  }
+
+  show() {
+    if (this.type == 0) fill(255, 0, 0);
+    // red power-up
+    else if (this.type == 1) fill(0, 255, 0);
+    // green power-up
+    else if (this.type == 2) fill(0, 0, 255);
+    // blue power-up
+    else if (this.type == 3) fill(255, 165, 0);
+    // orange power-up
+    else if (this.type == 4) fill(156, 39, 176); // purple power-up
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    this.y += this.speed;
+  }
+
+  checkCollision(paddle, i) {
+    // check if the power-up has collided with the paddle
+    if (
+      this.y + this.height >= paddle.y &&
+      this.x >= paddle.x &&
+      this.x + this.width <= paddle.x + paddle.width
+    ) {
+      this.activate(paddle, this.type); // apply the power-up effect
+      powerUps.splice(i, 1);
+      powerUpSound.play(); // play a sound effect
+      return true;
+    }
+  }
+
+  activate(paddle, type) {
+    if (type == 0) {
+      // red power-up: increase paddle size
+      paddle.width += 20;
+    } else if (type == 1) {
+      // green power-up: add extra life
+      lives++;
+    } else if (type == 2) {
+      // blue power-up: decrease paddle size
+      paddle.width -= 20;
+    } else if (type == 3) {
+      // orange power-up: increase ball speed
+      ball.ySpeed *= 2;
+    } else if (type == 4) {
+      // purple power-up: fire ball
+      ball.color = "red";
+      ball.ySpeed *= 1.3;
+      for (let i = 0; i < bricks.length; i++) {
+        let brick = bricks[i];
+        brick.damage = 2;
+      }
     }
   }
 }
